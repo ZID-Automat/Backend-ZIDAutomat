ac8463ae950a505120e5d302fba8a7b2f42efe7f
Maxi1324 <maxi1234567890fischer@gmail.com>
2022-10-11 20:56:56 +0200

    add Auth

---
+++ ZID.Automat/src/ZID.Automat.Api/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using ZID.Automat.Api.Models;
+using Microsoft.Net.Http.Headers;
+
+namespace ZID.Automat.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("Login")]
+        public async Task<object> OnPostAsync()
+        {
+            var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, "asd"),
+            new Claim("FullName", "asd"),
+            new Claim(ClaimTypes.Role, "Administrator"),
+        };
+
+            var claimsIdentity = new ClaimsIdentity(
+                claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties();
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
+
+            return "super";
+        }
+
+        [HttpGet("Logout")]
+        public async Task<object> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return "ja lol ey";
+        }
+    }
+}
--- ZID.Automat/src/ZID.Automat.Api/Program.cs
+++ ZID.Automat/src/ZID.Automat.Api/Program.cs
@@ -1,33 +1,69 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+using MvcJsonOptions = Microsoft.AspNetCore.Mvc.JsonOptions;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 string[] CorsOrigins = builder.Configuration.GetSection("CorsOrigin").Get<string[]>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddCors(
          option => option.AddDefaultPolicy(
              builder => builder.WithOrigins(CorsOrigins).AllowAnyHeader().AllowAnyMethod()
              )
          ); ;
 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, config =>
+    {
+        config.Cookie.Name = "UserLoginCookie";
+    });
+
+
+
+builder.Services.Configure<JsonOptions>(o => o.SerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+builder.Services.Configure<MvcJsonOptions>(o => o.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+builder.Services.AddCors(
+    option => option.AddDefaultPolicy(
+        builder => builder.WithOrigins(CorsOrigins).AllowAnyHeader().AllowAnyMethod()
+    )
+); 
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
     app.UseCors();
+
+    app.UseCookiePolicy();
+    app.UseAuthentication();
+    app.UseAuthorization();
 }
+
 
 app.UseCors();
 app.UseHttpsRedirection();
 
+app.UseCookiePolicy();
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
 
+
+app.MapControllers();
 app.Run();
