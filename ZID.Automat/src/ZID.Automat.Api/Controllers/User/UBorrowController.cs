e8647a33a901d3b63881932cb5287b623f317a48
Maximilian Fischer <54480672+Maxi1324@users.noreply.github.com>
2022-11-21 20:01:35 +0100

    Borrow Controller

--- ZID.Automat/src/ZID.Automat.Api/Controllers/User/UBorrowController.cs
+++ ZID.Automat/src/ZID.Automat.Api/Controllers/User/UBorrowController.cs
@@ -1,6 +1,13 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
+using System.Security.Claims;
+using System.Security.Principal;
+using ZID.Automat.Application;
+using ZID.Automat.Domain.Models;
+using ZID.Automat.Dto.Models;
+using ZID.Automat.Repository;
+
 namespace ZID.Automat.Api.Controllers.User
 {
     [ApiController]
@@ -8,9 +15,17 @@ namespace ZID.Automat.Api.Controllers.User
     [Route("[controller]")]
     public class UBorrowController
     {
-        public void Borrow()
+        public readonly IBorrowService _borrowService;
+        public UBorrowController(IBorrowService borrowService)
         {
-            
+            _borrowService = borrowService;
+        }
+
+        [HttpPost("Borrow")]
+        public void Borrow(BorrowDataDto borrowData)
+        {
+            var username = ClaimsPrincipal.Current?.Claims.First(c => c.Type == "Name")??throw new Exception("can't red UserName");
+            this._borrowService.Borrow(borrowData, username.Value, DateTime.Now);
         }
     }
 }
--- ZID.Automat/src/ZID.Automat.Api/Controllers/User/UItemController.cs
+++ ZID.Automat/src/ZID.Automat.Api/Controllers/User/UItemController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+
 using System.Security.Claims;
 using System.Security.Principal;
 using ZID.Automat.Application;
