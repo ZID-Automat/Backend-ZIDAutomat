aefd19a167b301b65d2d2d678c25d61716dac0cc
LuckyForce <adrian.schauer@aon.at>
2023-05-08 15:40:48 +0200

    AAnalyticsController Creation

---
+++ ZID.Automat/src/ZID.Automat.Api/Controllers/Admin/AAnalyticsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ZID.Automat.Application;
+
+namespace ZID.Automat.Api.Controllers.Admin
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AAnalyticsController : ControllerBase
+    {
+        public AAnalyticsController()
+        {
+        }
+
+        [HttpGet("AnalyticsItems")]
+        //[Authorize]
+        public IActionResult GetAnalyticsItems()
+        {
+            var items = AnalyticsService.GetAnalyticsItems();
+            return Ok(items);
+        }
+    }
+}
