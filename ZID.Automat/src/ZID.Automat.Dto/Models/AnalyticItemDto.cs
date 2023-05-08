53228f399c4e72c3eeee27002649d41106476246
LuckyForce <adrian.schauer@aon.at>
2023-05-08 15:46:32 +0200

    AnalyticsItemDto Creation

---
+++ ZID.Automat/src/ZID.Automat.Dto/Models/AnalyticItemDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZID.Automat.Dto.Models
+{
+    public class AnalyticItemDto
+    {
+        public string Name { get; set; } = default!;
+        public List<AnalyticItemMonth> Monate { get; set; } = new List<AnalyticItemMonth>();
+    }
+
+    public class AnalyticItemMonth
+    {
+        public string Name { get; set; } = default!;
+        public int Value { get; set; }
+    }
+}
